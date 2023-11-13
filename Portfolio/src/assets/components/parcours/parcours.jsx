bec0b5129f59e0cf10508eee16cf4d95bcc4e9de
t.bretones <titouan.bretones@gmail.com>
2023-11-13 13:47:02 +0100

    changement parcours

--- Portfolio/src/assets/components/parcours/parcours.jsx
+++ Portfolio/src/assets/components/parcours/parcours.jsx
@@ -7,7 +7,7 @@ function Parcours(){
             <div className='parcours__placement'>
                 <img src={Photo} className='parcours__photo' alt="Photo de Titouan Bretones" />
                 <p className='parcours__biographie'>Je suis Titouan Bretones, un développeur front-end passionné basé en Bretagne. J'ai récemment terminé ma formation d'intégrateur web chez OpenClassroom, 
-                    j'adore créer des expériences utilisateur innovantes. Mon travail reflète ma connexion à la Bretagne, une source constante d'inspiration.
+                    j'adore créer des expériences utilisateur innovantes.
                 </p>
             </div>
         </section>
