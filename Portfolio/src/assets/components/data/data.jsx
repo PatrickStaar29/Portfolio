ae764f1584fa38d2004ab1ce7c46589eb3bef3b9
t.bretones <titouan.bretones@gmail.com>
2023-11-21 15:25:18 +0100

    modification description sophie bluel

--- Portfolio/src/assets/components/data/data.jsx
+++ Portfolio/src/assets/components/data/data.jsx
@@ -53,7 +53,7 @@ const Tabl =[
             "title":"Sophie Bluel",
             "cover":"/data/image/sophiebluel.webp",
             "alt":"Photo de la page d'acceuil du site de Sophie Bluel",
-            "description":"Pour ce projet, j'ai simulé le rôle de développeur front-end pour Argent Bank, une banque en ligne fictive. J'ai conceptualisé un tableau de bord utilisateur avec React et Redux, basé sur des spécifications précises. J'ai proposé des routes API en utilisant Swagger et appliqué des principes du Green Code, notamment en optimisant les images et les composants.",
+            "description":"Ce projet consistait à intégrer les différentes pages du portfolio d'une architecte d'intérieur en utilisant des ressources telles que le design Figma, le code du back-end existant, et la base du front-end. Les principales tâches comprenaient l'intégration de la page de présentation de ses travaux à partir du HTML existant, l'implémentation de la page de connexion de l'administrateur, et le développement d'une modale pour l'upload et la suppression des projets.",
             "tags": [
                 "Html",
                 "Css",
